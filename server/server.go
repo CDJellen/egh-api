56111a06c6525caa5c7dfd934483598c2be2541a
Chris Jellen <cdjellen@gmail.com>
2023-02-19 15:58:14 -0800

    add new telemetry packages

--- server/server.go
+++ server/server.go
@@ -10,13 +10,6 @@ import (
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/rs/cors"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 	"google.golang.org/grpc"
 
 	"github.com/cdjellen/egh-api/app"
@@ -179,32 +172,6 @@ func (s *ContributorsServer) CreateContributors(ctx context.Context, req *pb.Cre
 
 func (s *ContributorsServer) UpdateContributors(ctx context.Context, req *pb.UpdateContributorsRequest) (*pb.UpdateContributorsResponse, error) {
 	return s.UpdateContributorsEndpoint(ctx, req)
-}
-
-func NewGrpcTraceProvider(ctx context.Context, serviceName string, traceAddr string, sampling float64) (provider *trace.TracerProvider, err error) {
-	batcher, err := otlptracegrpc.New(ctx, otlptracegrpc.WithEndpoint(traceAddr))
-	if err != nil {
-		log.Printf("failed to obtain grpc trace: %+v", err)
-		return nil, err
-	}
-
-	// define applicable resource tags
-	resources := resource.NewWithAttributes(semconv.SchemaURL, attribute.String("tracer address", fmt.Sprintf("%s", traceAddr)), attribute.String("service name", "egh-api"))
-
-	// define sampling rate
-	sampler := trace.ParentBased(trace.TraceIDRatioBased(sampling))
-
-	// define a new tracing provider
-	provider = trace.NewTracerProvider(trace.WithBatcher(batcher), trace.WithResource(resources), trace.WithSampler(sampler))
-
-	// define a new tracing propagator
-	propagator := propagation.NewCompositeTextMapPropagator(propagation.TraceContext{})
-
-	otel.SetTracerProvider(provider)
-	otel.SetTextMapPropagator(propagator)
-
-	return provider, nil
-
 }
 
 func Run(ctx context.Context, network string, address string, name string, healthServer *HealthServer, infoServer *InfoServer, contributionsServer *ContributionsServer, contributorsServer *ContributorsServer, readMeServer *ReadMeServer) error {
---
+++ server/telemetry/tracing/tracing.go
@@ -0,0 +1,41 @@
+package tracing
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+)
+
+func NewGrpcTraceProvider(ctx context.Context, serviceName string, traceAddr string, sampling float64) (provider *trace.TracerProvider, err error) {
+	batcher, err := otlptracegrpc.New(ctx, otlptracegrpc.WithEndpoint(traceAddr))
+	if err != nil {
+		log.Printf("failed to obtain grpc trace: %+v", err)
+		return nil, err
+	}
+
+	// define applicable resource tags
+	resources := resource.NewWithAttributes(semconv.SchemaURL, attribute.String("tracer address", fmt.Sprintf("%s", traceAddr)), attribute.String("service name", "egh-api"))
+
+	// define sampling rate
+	sampler := trace.ParentBased(trace.TraceIDRatioBased(sampling))
+
+	// define a new tracing provider
+	provider = trace.NewTracerProvider(trace.WithBatcher(batcher), trace.WithResource(resources), trace.WithSampler(sampler))
+
+	// define a new tracing propagator
+	propagator := propagation.NewCompositeTextMapPropagator(propagation.TraceContext{})
+
+	otel.SetTracerProvider(provider)
+	otel.SetTextMapPropagator(propagator)
+
+	return provider, nil
+
+}
